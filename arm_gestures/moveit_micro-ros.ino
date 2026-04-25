6a11263dc8bf06839dcb80484990d853dfb7f332
Siddhant-83-74 <siddhantjain8104@gmail.com>
2026-04-25 21:16:10 +0530

    Temp

--- arm_gestures/moveit_micro-ros.ino
+++ arm_gestures/moveit_micro-ros.ino
@@ -48,8 +48,8 @@ std::vector<CubeMarsMotor> leftArm = {
 std::vector<CubeMarsMotor> rightArm = {
   CubeMarsMotor(0x05, rightBus, 6, 4000, 6000),
   CubeMarsMotor(0x06, rightBus, 6, 4000, 6000),
-  CubeMarsMotor(0x07, rightBus, 6, 4000, 6000),
   CubeMarsMotor(0x08, rightBus, 6, 4000, 6000),
+  CubeMarsMotor(0x02, rightBus, 6, 4000, 6000),
 };
 bool g_originSet = false;   // false until we run setOrigin() once
 
@@ -155,28 +155,57 @@ const Gesture GESTURE_HOME = {
   .count = 1,
 };
 
+// const Gesture GESTURE_HELLO = {
+//   .frames = {
+//     // 1. Raise right arm
+//     { .joints = {0, 0, 0, 0, 0, 0,   48, 3, 0, -107, 91, 35},      .duration_ms = 5000, .speed_erpm = 5000 },
+//     // 2. Wave right (wrist + forearm)
+//     { .joints = {0, 0, 0, 0, 0, 0,   48, 3, -8, -107, 91, 35},    .duration_ms = 1000, .speed_erpm = 5000 },
+//     // 3. Wave left
+//     { .joints = {0, 0, 0, 0, 0, 0,   48, 3, 22, -107, 91, 35},  .duration_ms = 1000, .speed_erpm = 5000 },
+//     // 4. Wave right again
+//     { .joints = {0, 0, 0, 0, 0, 0,   48, 3,-13, -107, 91, 35},    .duration_ms = 1000, .speed_erpm = 5000 },
+//     // 5. Return to rest
+//     { .joints = {0, 0, 0, 0, 0, 0,   48, 3,26, -107, 91, 35},    .duration_ms = 1000, .speed_erpm = 5000 },
+
+//     { .joints = {0, 0, 0, 0, 0, 0,   48, 3, 13, -107, 91, 35},    .duration_ms = 1000, .speed_erpm = 5000 },
+
+//     { .joints = {0, 0, 0, 0, 0, 0,   48, 2,13, -107, 91, 35},    .duration_ms = 1000, .speed_erpm = 5000 },
+
+//     { .joints = {0, 0, 0, 0, 0, 0,   48, 2,10, -70, 91, 35},    .duration_ms = 1000, .speed_erpm = 5000 },
+
+//     { .joints = {0, 0, 0, 0, 0, 0,   24, 2,6, -50, 45, 20},    .duration_ms = 1000, .speed_erpm = 5000 },
+
+//     { .joints = {0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0},           .duration_ms = 1000, .speed_erpm = 3000 },
+//   },
+//   .count = 10,
+// };
+
+
+
 const Gesture GESTURE_HELLO = {
   .frames = {
     // 1. Raise right arm
-    { .joints = {0, 0, 0, 0, 0, 0,   48, 3, 0, -107, 91, 35},      .duration_ms = 5000, .speed_erpm = 5000 },
+    { .joints = {-34, 8, -1, -117, 0, 0,   0, 0, 0, 0, 0, 0},      .duration_ms = 5000, .speed_erpm = 5000 },
     // 2. Wave right (wrist + forearm)
-    { .joints = {0, 0, 0, 0, 0, 0,   48, 3, -8, -107, 91, 35},    .duration_ms = 1000, .speed_erpm = 5000 },
-    // 3. Wave left
-    { .joints = {0, 0, 0, 0, 0, 0,   48, 3, 22, -107, 91, 35},  .duration_ms = 1000, .speed_erpm = 5000 },
-    // 4. Wave right again
-    { .joints = {0, 0, 0, 0, 0, 0,   48, 3,-13, -107, 91, 35},    .duration_ms = 1000, .speed_erpm = 5000 },
-    // 5. Return to rest
-    { .joints = {0, 0, 0, 0, 0, 0,   48, 3,26, -107, 91, 35},    .duration_ms = 1000, .speed_erpm = 5000 },
+    { .joints = {-34, 8, -10, -117, 0, 0,   0, 0, 0, 0, 0, 0},      .duration_ms = 1000, .speed_erpm = 5000 },
 
-    { .joints = {0, 0, 0, 0, 0, 0,   48, 3, 13, -107, 91, 35},    .duration_ms = 1000, .speed_erpm = 5000 },
+    { .joints = {-34, 8, 20, -117, 0, 0,   0, 0, 0, 0, 0, 0},      .duration_ms = 1000, .speed_erpm = 5000 },
 
-    { .joints = {0, 0, 0, 0, 0, 0,   48, 2,13, -107, 91, 35},    .duration_ms = 1000, .speed_erpm = 5000 },
+    { .joints = {-34, 8, -24, -117, 0, 0,   0, 0, 0, 0, 0, 0},      .duration_ms = 1000, .speed_erpm = 5000 },
 
-    { .joints = {0, 0, 0, 0, 0, 0,   48, 2,10, -70, 91, 35},    .duration_ms = 1000, .speed_erpm = 5000 },
+    { .joints = {-34, 8, 24, -117, 0, 0,   0, 0, 0, 0, 0, 0},      .duration_ms = 1000, .speed_erpm = 5000 },
 
-    { .joints = {0, 0, 0, 0, 0, 0,   24, 2,6, -50, 45, 20},    .duration_ms = 1000, .speed_erpm = 5000 },
+    { .joints = {-34, 8, 10, -117, 0, 0,   0, 0, 0, 0, 0, 0},      .duration_ms = 1000, .speed_erpm = 5000 },
 
-    { .joints = {0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0},           .duration_ms = 1000, .speed_erpm = 3000 },
+    { .joints = {-34, 8, 10, -117, 0, 0,   0, 0, 0, 0, 0, 0},      .duration_ms = 1000, .speed_erpm = 5000 },
+
+    { .joints = {-25, 4, 5, -80, 0, 0,   0, 0, 0, 0, 0, 0},      .duration_ms = 1000, .speed_erpm = 5000 },
+
+    { .joints = {-10, 2, 5, -30, 0, 0,   0, 0, 0, 0, 0, 0},      .duration_ms = 1000, .speed_erpm = 5000 },
+
+    { .joints = {0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0},      .duration_ms = 1000, .speed_erpm = 3000 },
+
   },
   .count = 10,
 };
